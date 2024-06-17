55988974c967bc421da61826a31e06c5de0eb3cf
jasminshaik6866 <jasminshaik6866@gmail.com>
2024-06-17 20:20:46 +0530

    created new repo

---
+++ src/main/java/com/example/accountproduct/AccountproductApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountproductApplication.class, args);
+	}
+
+}
